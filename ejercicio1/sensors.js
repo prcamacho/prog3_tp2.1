d089cc4f8c4e1ea2bfde7976563ca2f3f0d2c07f
Pablo <pablocamacho92@gmail.com>
2024-06-01 14:10:17 -0300

    Avance punto 1

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,56 +1,83 @@
 class Sensor {
-    constructor(id, name, type, value, unit, updatedAt){
-        this.id;
-        this.name;
-        this.type = `temperature` || `humidity` || `pressure`;
-        this.value;
-        this.unit;
-        this.updatedAt;
-    }
+  constructor(id, name, type, value, unit, updatedAt) {
+    this.id = id;
+    this.name = name;
+    this.conditionType(type);
+    this.value = value;
+    this.unit = unit;
+    this.updatedAt = updatedAt;
+  }
+
+  conditionType(type) {
+    const conditions = ["temperature", "humidity", "pressure"];
+    conditions.forEach((element) => {
+      if (type === element) {
+        console.log("Valor ingresado correctamente.");
+        this.value = type;
+      }
+    });
+  }
+
+  set updateValue(value) {
+    this.value = value;
+    this.updatedAt = new Date().toString();
+  }
 }
 
 class SensorManager {
-    constructor() {
-        this.sensors = [];
+  constructor() {
+    this.sensors = [];
+  }
+
+  addSensor(sensor) {
+    this.sensors.push(sensor);
+  }
+
+  updateSensor(id) {
+    const sensor = this.sensors.find((sensor) => sensor.id === id);
+    if (sensor) {
+      let newValue;
+      switch (sensor.type) {
+        case "temperatura": // Rango de -30 a 50 grados Celsius
+          newValue = (Math.random() * 80 - 30).toFixed(2);
+          break;
+        case "humedad": // Rango de 0 a 100%
+          newValue = (Math.random() * 100).toFixed(2);
+          break;
+        case "presion": // Rango de 960 a 1040 hPa (hectopascales o milibares)
+          newValue = (Math.random() * 80 + 960).toFixed(2);
+          break;
+        default: // Valor por defecto si el tipo es desconocido
+          newValue = (Math.random() * 100).toFixed(2);
+      }
+      sensor.updateValue = newValue;
+      console.log(newValue);
+      this.render();
+    } else {
+      console.error(`Sensor ID ${id} no encontrado`);
     }
+  }
 
-    addSensor(sensor) {
-        this.sensors.push(sensor);
-    }
+  async loadSensors(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((sensor) => {
+          this.addSensor(sensor);
+        });
+      })
+      .finally(() => {
+        this.render();
+      });
+  }
 
-    updateSensor(id) {
-        const sensor = this.sensors.find((sensor) => sensor.id === id);
-        if (sensor) {
-            let newValue;
-            switch (sensor.type) {
-                case "temperatura": // Rango de -30 a 50 grados Celsius
-                    newValue = (Math.random() * 80 - 30).toFixed(2);
-                    break;
-                case "humedad": // Rango de 0 a 100%
-                    newValue = (Math.random() * 100).toFixed(2);
-                    break;
-                case "presion": // Rango de 960 a 1040 hPa (hectopascales o milibares)
-                    newValue = (Math.random() * 80 + 960).toFixed(2);
-                    break;
-                default: // Valor por defecto si el tipo es desconocido
-                    newValue = (Math.random() * 100).toFixed(2);
-            }
-            sensor.updateValue = newValue;
-            this.render();
-        } else {
-            console.error(`Sensor ID ${id} no encontrado`);
-        }
-    }
-
-    async loadSensors(url) {}
-
-    render() {
-        const container = document.getElementById("sensor-container");
-        container.innerHTML = "";
-        this.sensors.forEach((sensor) => {
-            const sensorCard = document.createElement("div");
-            sensorCard.className = "column is-one-third";
-            sensorCard.innerHTML = `
+  render() {
+    const container = document.getElementById("sensor-container");
+    container.innerHTML = "";
+    this.sensors.forEach((sensor) => {
+      const sensorCard = document.createElement("div");
+      sensorCard.className = "column is-one-third";
+      sensorCard.innerHTML = `
                 <div class="card">
                     <header class="card-header">
                         <p class="card-header-title">
@@ -69,29 +96,29 @@ class SensorManager {
                         </div>
                         <time datetime="${sensor.updated_at}">
                             Última actualización: ${new Date(
-                                sensor.updated_at
+                              sensor.updated_at
                             ).toLocaleString()}
                         </time>
                     </div>
                     <footer class="card-footer">
                         <a href="#" class="card-footer-item update-button" data-id="${
-                            sensor.id
+                          sensor.id
                         }">Actualizar</a>
                     </footer>
                 </div>
             `;
-            container.appendChild(sensorCard);
-        });
+      container.appendChild(sensorCard);
+    });
 
-        const updateButtons = document.querySelectorAll(".update-button");
-        updateButtons.forEach((button) => {
-            button.addEventListener("click", (event) => {
-                event.preventDefault();
-                const sensorId = parseInt(button.getAttribute("data-id"));
-                this.updateSensor(sensorId);
-            });
-        });
-    }
+    const updateButtons = document.querySelectorAll(".update-button");
+    updateButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        const sensorId = parseInt(button.getAttribute("data-id"));
+        this.updateSensor(sensorId);
+      });
+    });
+  }
 }
 
 const monitor = new SensorManager();
